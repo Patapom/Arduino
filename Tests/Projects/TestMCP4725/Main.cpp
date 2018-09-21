3b9a45d418015ebb222b7d5770fbfa47d35bfecb
Patapom <patapom@gmail.com>
2018-09-21 12:51:19 +0200

    • Added a R-2R resistor ladder DAC test project + board  => Working super fine!  => In the process of soldering an actual board

--- Tests/Projects/TestMCP4725/Main.cpp
+++ Tests/Projects/TestMCP4725/Main.cpp
@@ -1,3 +1,12 @@
+//////////////////////////////////////////////////////////////////////////
+// Tests the MCP4725 serial DAC
+// It turns out that serial transmission is much too slow to accomodate our 16KHz audio target
+// Instead, we'll use Amanda Ghassaei's R-2R resistor ladder DAC tp start with so only a single write to PORTD
+//	is necessary to obtain a nice conversion. The DAC is 8-bits and a bit subject to noise but I followed her
+//	advice and ordered a bunch of TLC7528 that are high-precision dual channel DACs and they'll hopefully
+//	be able to meet our needs...
+//////////////////////////////////////////////////////////////////////////
+//
 #include "Pom/Pom.h"
 
 TWI	twi;
@@ -8,22 +17,15 @@ void setup2() {
 	MCP4725_Begin( twi, 0x60 );	// Device Code = 0x60 | {A2, A1,A0} as LSB.
 								// By default, A2 = 0, A1 = 0, A0 = 0.
 								// A0 can be dynamically chosen by the user to be GND(0) or VCC(1)
-//	MCP4725_SetValueEEPROM( twi, 0 );	// Default is 0!
+
+//	MCP4725_SetValueEEPROM( twi, 0 );	// Default should be 0!
 }
 
 void loop() {
 
 	// Write a sine-wave in fast mode (i.e. only 2 bytes required)
 	static U32	counter = 0;
-	U16			value = (U16) (2047 * (1.0f + cos( (2*PI * counter++) / 16 )));
-
-//value = 4095;
-
-// 				value |= ((BIT_PD0 << 1) | BIT_PD1) << 12;
-// 
-// SwapBytes( value );
-// 
-// 	twi.Push( (const U8*) &value, 2 );
+	U16			value = (U16) (2047 * (1.0f + cos( (2*PI * counter++) / 2 )));
 
 	MCP4725_SetValue( twi, value );
 
---
+++ Tests/Projects/TestR2RDAC/Main.cpp
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////
+// R-2R Resistor Ladder DAC Test
+// From Amanda Ghassaei's project (https://www.instructables.com/id/Arduino-Audio-Output/)
+//////////////////////////////////////////////////////////////////////////
+//
+// Check noise reduction LC circuit from section 24.6.1 when plugging something into an analog input
+
+#include "Pom/Pom.h"
+
+void setup2() {
+	for ( int i=0; i < 8; i++ )
+		pinMode( i, OUTPUT );
+}
+
+void loop() {
+	static U8	counter = 0;
+	PORTD = counter+=16;
+//	delay( 10 );
+	delayMicroseconds( 10 );
+}
